001e8fda97ab82ee965ed672984c0a02658ff551
Parth Vijay <parthvj1998@gmail.com>
2020-07-16 15:39:53 +0530

    merged

--- pdf2html.py
+++ pdf2html.py
@@ -2,10 +2,7 @@
 # pip3 install PyMuPDF
 # pip3 install fitz
 import sys, fitz
-print("hello")
-print("hi")
 pdf = "sample.pdf"
-
 doc = fitz.open(pdf)
 for page in doc:
     html_text = page.getText("html")
