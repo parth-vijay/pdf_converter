3544f886c8db6affbb9e4aef00e420faaeb59eb8
Parth Vijay <parthvj1998@gmail.com>
2020-07-16 10:55:12 +0530

    spaces removed

--- pdf2html.py
+++ pdf2html.py
@@ -2,14 +2,8 @@
 # pip3 install PyMuPDF
 # pip3 install fitz
 import sys, fitz
-
-
 print("hello")
-
-print("hi")
-
 pdf = "sample.pdf"
-
 doc = fitz.open(pdf)
 for page in doc:
     html_text = page.getText("html")
