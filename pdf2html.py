1202d0ca0e0284d8e57108f58571ee8d204aa495
Parth Vijay <parthvj1998@gmail.com>
2020-07-16 17:37:55 +0530

    removed unnecessary

--- pdf2html.py
+++ pdf2html.py
@@ -2,19 +2,9 @@
 # pip3 install PyMuPDF
 # pip3 install fitz
 import sys, fitz, os
-print("hi there")
-print("hello")
 pdf = "sample.pdf"
 doc = fitz.open(pdf)
 for page in doc:
     html_text = page.getText("html")
 with open('output.html', 'w') as f:
     f.write(html_text)
-
-print("this is a test")
-
-
-print("this is another test")
-
-
-print("this is 3rd test")
